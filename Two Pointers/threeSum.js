8327253aa2cf3f8d44d73011677e045b0f7d57ae
Yash UNHAWANE <74054179+yashunhawane@users.noreply.github.com>
2026-03-05 17:26:42 +0530

    Refactor threeSum function implementation
    Refactor threeSum function for improved readability and efficiency.

--- Two Pointers/threeSum.js
+++ Two Pointers/threeSum.js
@@ -1,46 +1,34 @@
-let nums = [-1, 0, 1, 2, -1, -4]
+let nums = [-1,0,1,2,-1,-4]
 
-
-let threeSum = (nums) => {
-    nums.sort((a, b) => a - b);
-    let res = [];
-    let n = nums.length;
-
-    for (let i = 0; i < n - 1; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) continue
-
-        let left = i + 1
-        let right = n - 1
-        let target = -nums[i]
-
-
-        while (left < right) {
+let threeSum = (nums)=>{
+    nums.sort((a,b)=> a-b)
+    let  res = []
+    for(let i= 0 ; i <nums.length-2; i++){
+        if(i>0 &&nums[i]==nums[i-1])continue
+        let left = i+1
+        let right = nums.length-1
+        let target = -1 * nums[i]
+        
+        while(left < right){
             let sum = nums[left] + nums[right]
-            if (sum == target) {
-                res.push([nums[i], nums[left], nums[right]])
-
-                left++
+            if( sum == target){
+               res.push([nums[i], nums[left], nums[right]]) 
+               left++
                 right--
-
-                while (nums[left] == nums[left - 1]) {
-                    left++
-                }
-                while (nums[right] == nums[right + 1]) {
-                    right--
-                }
-            } else if (sum < target) {
-                left++
-            } else {
-                right--
+               while(left < right && nums[left] == nums[left-1]){
+                   left ++
+               }
+                while(left < right && nums[right] == nums[right+1]){
+                   right --
+               }
+            }else if(sum < target){
+                left ++
+            }else{
+                right --
             }
         }
-
-    }
-
-    return res;
-};
-
+    }return res
+}
 
 let ans = threeSum(nums)
-
 console.log(ans)
